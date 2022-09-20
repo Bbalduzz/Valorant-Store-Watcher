6e13c70555ae7910254c1dc32e6d4e83881c0a60
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-09-20 23:34:35 +0200

    Update valostorewatcher.py

--- valostorewatcher.py
+++ valostorewatcher.py
@@ -1,3 +1,4 @@
+## WATCHER IMPORTS ###
 from requests import session as sesh, get
 from requests.adapters import HTTPAdapter
 from ssl import PROTOCOL_TLSv1_2
@@ -9,6 +10,38 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 from rich.table import Table, box
 from rich.console import Console
+
+## GUI IMPORTS ##
+from pathlib import Path
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from PIL import ImageTk, Image
+import json
+from io import BytesIO
+import urllib.request
+from datetime import date
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--gui", help = "Display or not the GUI window")
+args = vars(parser.parse_args())
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./assets")
+
+def url_image(link, size):
+    with urllib.request.urlopen(link) as u:
+        raw_data = u.read()
+    if size == 'big':
+        pixels_x, pixels_y = 547, 237
+    elif size == 'long':
+        pixels_x, pixels_y = 150, 45
+    else:
+        pixels_x, pixels_y = 100, 45
+    image = Image.open(BytesIO(raw_data)).resize((pixels_x, pixels_y))
+    return ImageTk.PhotoImage(image)
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
 
 class TLSAdapter(HTTPAdapter):
     def init_poolmanager(self, connections, maxsize, block=False):
@@ -28,6 +61,291 @@ def price_retriver(skinUuid, offers_data):
             for cost in row["Cost"]:
                 return row["Cost"][cost]
 
+
+def MainGui():
+    window = Tk()
+    window.geometry("779x417")
+    window.configure(bg = "#081527")
+    window.title('ValorantStoreWatcher')
+
+    # bundle_name
+    # bundle_image
+    # bundle_price
+    # skin1_image
+    # skin1_name
+    # skin1_price
+    # skin2_image
+    # skin2_name
+    # skin2_price
+    # skin3_image
+    # skin3_name
+    # skin3_price
+    # skin4_image
+    # skin4_name
+    # skin4_price
+    # valorant_points_amount
+    # radianite_points_amount
+
+    dailyshop = json.load(open('dailyshop.json'))
+    bundle_name = dailyshop['dailyshop'][0]['Bundle']['bundle_name']
+    bundle_image = dailyshop['dailyshop'][0]['Bundle']['bundle_image']
+    bundle_price = dailyshop['dailyshop'][0]['Bundle']['bundle_price']
+    skin1_image = dailyshop['dailyshop'][0]['Skins']['skin1']['skin1_image']
+    skin1_name = dailyshop['dailyshop'][0]['Skins']['skin1']['skin1_name']
+    skin1_price = dailyshop['dailyshop'][0]['Skins']['skin1']['skin1_price']
+    skin2_image = dailyshop['dailyshop'][0]['Skins']['skin2']['skin2_image']
+    skin2_name = dailyshop['dailyshop'][0]['Skins']['skin2']['skin2_name']
+    skin2_price = dailyshop['dailyshop'][0]['Skins']['skin2']['skin2_price']
+    skin3_image = dailyshop['dailyshop'][0]['Skins']['skin3']['skin3_image']
+    skin3_name = dailyshop['dailyshop'][0]['Skins']['skin3']['skin3_name']
+    skin3_price = dailyshop['dailyshop'][0]['Skins']['skin3']['skin3_price']
+    skin4_image = dailyshop['dailyshop'][0]['Skins']['skin4']['skin4_image']
+    skin4_name = dailyshop['dailyshop'][0]['Skins']['skin4']['skin4_name']
+    skin4_price = dailyshop['dailyshop'][0]['Skins']['skin4']['skin4_price']
+    valorant_points_amount = dailyshop['dailyshop'][0]['ValorantPoints_amount']
+    radianite_points_amount = dailyshop['dailyshop'][0]['RadianitePoints_amount']
+
+
+
+    canvas = Canvas(
+        window,
+        bg = "#081527",
+        height = 417,
+        width = 779,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge"
+    )
+
+    canvas.place(x = 0, y = 0)
+    canvas.create_text(
+        22.0,
+        288.0,
+        anchor="nw",
+        text="VALORANT",
+        fill="#DC3D4B",
+        font=("VALORANT", 64 * -1)
+    )
+
+    canvas.create_text(
+        197.0,
+        350.0,
+        anchor="nw",
+        text="SHOP",
+        fill="#DC3D4B",
+        font=("VALORANT", 64 * -1)
+    )
+
+    image_image_1 = url_image(bundle_image, 'big')
+    image_1 = canvas.create_image(
+        295.0,
+        146.0,
+        image=image_image_1
+    )
+
+    image_image_2 = PhotoImage(
+        file=relative_to_assets("valopoints.png"))
+    image_2 = canvas.create_image(
+        610.0,
+        180.0,
+        image=image_image_2
+    )
+
+    canvas.create_text(
+        628.0,
+        175.0,
+        anchor="nw",
+        text=skin2_price,
+        fill="#FFFFFF",
+        font=("VALORANT", 13 * -1)
+    )
+
+    image_image_3 = PhotoImage(
+        file=relative_to_assets("valopoints.png"))
+    image_3 = canvas.create_image(
+        610.0,
+        300.0,
+        image=image_image_3
+    )
+
+    canvas.create_text(
+        628.0,
+        295.0,
+        anchor="nw",
+        text=skin3_price,
+        fill="#FFFFFF",
+        font=("VALORANT", 13 * -1)
+    )
+
+    canvas.create_text(
+        601.0,
+        375.0,
+        anchor="nw",
+        text=skin3_name,
+        fill="#FFFFFF",
+        font=("VALORANT", 12 * -1)
+    )
+
+    canvas.create_text(
+        417.0,
+        375.0,
+        anchor="nw",
+        text=skin4_name,
+        fill="#FFFFFF",
+        font=("VALORANT", 12 * -1)
+    )
+
+    image_image_4 = url_image(skin1_image, 'melee')
+    image_4 = canvas.create_image(
+        663.0,
+        73.0,
+        image=image_image_4
+    )
+
+    image_image_5 = PhotoImage(file=relative_to_assets("valopoints.png"))
+    image_5 = canvas.create_image(
+        610.0,
+        50.0,
+        image=image_image_5
+    )
+
+    canvas.create_text(
+        628.0,
+        45.0,
+        anchor="nw",
+        text=skin1_price,
+        fill="#FFFFFF",
+        font=("VALORANT", 13 * -1)
+    )
+
+    canvas.create_text(
+        681.0,
+        11.0,
+        anchor="nw",
+        text=date.today().strftime("%d/%m/%Y"),
+        fill="#FFFFFF",
+        font=("VALORANT", 12 * -1)
+    )
+
+    image_image_6 = PhotoImage(
+        file=relative_to_assets("valopoints.png"))
+    image_6 = canvas.create_image(
+        64.0,
+        366.0,
+        image=image_image_6
+    )
+
+    image_image_7 = PhotoImage(
+        file=relative_to_assets("valopoints.png"))
+    image_7 = canvas.create_image(
+        535.0,
+        244.0,
+        image=image_image_7
+    )
+
+    image_image_8 = PhotoImage(
+        file=relative_to_assets("radianite.png"))
+    image_8 = canvas.create_image(
+        64.0,
+        391.0,
+        image=image_image_8
+    )
+
+    canvas.create_text(
+        82.0,
+        361.0,
+        anchor="nw",
+        text=valorant_points_amount,
+        fill="#FFFFFF",
+        font=("VALORANT", 13 * -1)
+    )
+
+    image_image_9 = PhotoImage(
+        file=relative_to_assets("valopoints.png"))
+    image_9 = canvas.create_image(
+        419.0,
+        300.0,
+        image=image_image_9
+    )
+
+    canvas.create_text(
+        437.0,
+        295.0,
+        anchor="nw",
+        text=skin4_price,
+        fill="#FFFFFF",
+        font=("VALORANT", 13 * -1)
+    )
+
+    canvas.create_text(
+        486.0,
+        237.0,
+        anchor="nw",
+        text=bundle_price,
+        fill="#FFFFFF",
+        font=("VALORANT", 15 * -1)
+    )
+
+    canvas.create_text(
+        82.0,
+        386.0,
+        anchor="nw",
+        text=radianite_points_amount,
+        fill="#FFFFFF",
+        font=("VALORANT", 13 * -1)
+    )
+
+    canvas.create_text(
+        40.0,
+        229.0,
+        anchor="nw",
+        text=bundle_name,
+        fill="#FFFFFF",
+        font=("VALORANT", 24 * -1)
+    )
+
+    canvas.create_text(
+        601.0,
+        103.0,
+        anchor="nw",
+        text=skin1_name,
+        fill="#FFFFFF",
+        font=("VALORANT", 12 * -1)
+    )
+
+    canvas.create_text(
+        601.0,
+        236.0,
+        anchor="nw",
+        text=skin2_name,
+        fill="#FFFFFF",
+        font=("VALORANT", 12 * -1)
+    )
+
+    image_image_10 = url_image(skin2_image, 'mel')
+    image_10 = canvas.create_image(
+        668.0,
+        209.0,
+        image=image_image_10
+    )
+
+    image_image_11 = url_image(skin3_image, 'long')
+    image_11 = canvas.create_image(
+        670.9556884765625,
+        341.7883071899414,
+        image=image_image_11
+    )
+
+    image_image_12 = url_image(skin4_image, 'mel')
+    image_12 = canvas.create_image(
+        476.0,
+        341.0,
+        image=image_image_12
+    )
+    window.resizable(False, False)
+    window.mainloop()
+
+## VALOSTORE WATCHER ##
 def checker():
     acc =  config['LOGIN']
     username = acc['riot_username']
@@ -150,11 +468,13 @@ def checker():
         skin_videos.append(data['data']['streamedVideo']) # videos of daily items
 
     ## bundles part 2 ##
+    bundles_images = []
     current_bundles = []
     for bundle in bundles_uuid:
         with session.get(f'https://valorant-api.com/v1/bundles/{bundle}', headers=headers) as r:
             data = r.json()
         current_bundles.append(data['data']['displayName'])
+        bundles_images.append(data['data']['displayIcon'])
 
     ## DISPLAY ##
     table_one = Table(box=box.HORIZONTALS, show_header=True, header_style='bold #2070b2')
@@ -183,5 +503,48 @@ def checker():
     table.add_row(table_one, table_two)
     console = Console()
     console.print(table)
+
+    # for GUI
+    if args['gui']:
+        def write_json(new_data, filename='dailyshop.json'):
+            with open(filename, 'r+') as file:
+                file_data = json.load(file)
+                file_data['dailyshop'].append(new_data)
+                file.seek(0)
+                json.dump(file_data, file, indent=4)
+        
+        shop = {
+            "Bundle":{
+                "bundle_name": current_bundles[0],
+                "bundle_image": bundles_images[0],
+                "bundle_price": str(bundle_prices[0])
+            },
+            "Skins":{
+                "skin1":{
+                    "skin1_name": skin_names[0],
+                    "skin1_image": skin_images[0],
+                    "skin1_price": singleweapons_prices[0],
+                },
+                "skin2":{
+                    "skin2_name": skin_names[1],
+                    "skin2_image": skin_images[1],
+                    "skin2_price": singleweapons_prices[1],
+                },
+                "skin3":{
+                    "skin3_name": skin_names[2],
+                    "skin3_image": skin_images[2],
+                    "skin3_price": singleweapons_prices[2],
+                },
+                "skin4":{
+                    "skin4_name": skin_names[3],
+                    "skin4_image": skin_images[3],
+                    "skin4_price": singleweapons_prices[3],
+                }
+            },
+            "ValorantPoints_amount": ValorantPoints,
+            "RadianitePoints_amount": Radianite,
+        }
+        write_json(shop)
+        MainGui()
 
 checker()
